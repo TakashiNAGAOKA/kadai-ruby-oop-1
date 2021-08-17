292cc38b41edd5aefcd356a548be69ed52a3751d
takashi.nagaoka <takashi.nagaoka@gmail.com>
2021-08-17 06:15:44 +0000

    modify team.rb

--- team.rb
+++ team.rb
@@ -3,11 +3,11 @@ class Team
   attr_accessor :name, :win, :lose, :draw
 
   # 初期化用の特別なメソッド
-  def initialize
-    self.name = 'Giants'
-    self.win = 67
-    self.lose = 45
-    self.draw = 8
+  def initialize(t_name,t_win,t_lose,t_draw)
+    self.name = t_name
+    self.win = t_win
+    self.lose = t_lose
+    self.draw = t_draw
 #    p name
   end
 
@@ -22,41 +22,16 @@ class Team
     end
 end
 
-team_A = Team.new
-
-team_B = Team.new
- team_B.name = 'Tigers'
- team_B.win = 60
- team_B.lose = 53
- team_B.draw = 7
-
-team_C = Team.new
- team_C.name = 'Dragons'
- team_C.win = 60
- team_C.lose = 55
- team_C.draw = 5
-
-team_D = Team.new
- team_D.name = 'BayStars'
- team_D.win = 56
- team_D.lose = 58
- team_D.draw = 6
-
-team_E = Team.new
- team_E.name = 'Carp'
- team_E.win = 52
- team_E.lose = 56
- team_E.draw = 12
-
-team_E = Team.new
- team_E.name = 'Swallows'
- team_E.win = 41
- team_E.lose = 69
- team_E.draw = 10
+team_A   = Team.new("Giants",   67, 45,  8)
+team_B   = Team.new("Tigers",   60, 53,  7)
+team_C   = Team.new("Dragons",   60, 55,  5)
+team_D   = Team.new("BayStars",   56, 58,  6)
+team_E   = Team.new("Carp",   52, 56, 12)
+team_F   = Team.new("Swallows",   41, 69, 10)
 
 team_A.show_team_result
 team_B.show_team_result
 team_C.show_team_result
 team_D.show_team_result
 team_E.show_team_result
-
+team_F.show_team_result
